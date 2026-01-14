74caf8f44aa3fb666c8443c8edd243542483b0af
wayweaerd <phic.glc@gmail.com>
2026-01-14 09:39:23 +0300

    Added basic backend program

---
+++ backend/program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program{
+  static void Main(){
+        Console.WriteLine("Привет");
+        Console.WriteLine("Жанатпаева Динара Мурадымовна");
+        Console.WriteLine(DateTime.Now);
+  }
+}
