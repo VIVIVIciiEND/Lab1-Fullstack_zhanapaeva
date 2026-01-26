780f97f8e93257ba966326de104b6a9025114a2d
VIVIVIciiEND <akirmandinara@gmail.com>
2026-01-26 14:38:28 +0300

    333

--- backend/program.cs
+++ backend/program.cs
@@ -4,5 +4,21 @@ class Program{
         Console.WriteLine("Привет");
         Console.WriteLine("Жанатпаева Динара Мурадымовна");
         Console.WriteLine(DateTime.Now);
+        string? text = Console.ReadLine();
+        Console.WriteLine($"привет {text}");
+        DayOfWeek day = DateTime.Now.DayOfWeek;
+        Console.WriteLine(day);
+        Console.WriteLine("хотите продолжить? y/n");
+        string? w = Console.ReadLine();
+        if (w == "y")
+    {
+      Console.WriteLine("продолжим");
+    }
+    else if (w == "n")
+    {
+      Console.WriteLine("удачи");
+      
+    }
+
   }
 }
